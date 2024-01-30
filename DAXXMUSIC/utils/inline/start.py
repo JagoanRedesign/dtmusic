3e285666cb2ed91bc83ccbaa3a985daa10e7160b
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-30 19:26:01 +0700

    Update start.py

--- DAXXMUSIC/utils/inline/start.py
+++ DAXXMUSIC/utils/inline/start.py
@@ -33,3 +33,17 @@ def private_panel(_):
         ],
     ]
     return buttons
+
+
+def ping_panel(_):
+    buttons = [
+        
+        [
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+        ],
+        [
+            InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper"),
+        ],
+    ]
+    return buttons
