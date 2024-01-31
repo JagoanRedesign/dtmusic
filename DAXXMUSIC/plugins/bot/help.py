972e05d368b6e4e066c1b3e5c8476d7c658d4cfa
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-31 19:25:31 +0700

    Update help.py

--- DAXXMUSIC/plugins/bot/help.py
+++ DAXXMUSIC/plugins/bot/help.py
@@ -28,7 +28,7 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_, True)
         await update.edit_message_text(
-            _["help_1"].format(message.from_user.mention), reply_markup=keyboard
+            _["help_1"].format(update.message.from_user.mention), reply_markup=keyboard
         )
     else:
         try:
@@ -40,7 +40,7 @@ async def helper_private(
         keyboard = help_pannel(_)
         await update.reply_photo(
             photo=START_IMG_URL,
-            caption=_["help_1"].format(message.from_user.mention),
+            caption=_["help_1"].format(update.message.from_user.mention),
             reply_markup=keyboard,
         )
 
