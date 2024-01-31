891c563a5d90cb7d0c727a5113fad96f152f8c7c
JagoanRedesign <70496030+JagoanRedesign@users.noreply.github.com>
2024-01-31 12:49:51 +0700

    Update help.py

--- DAXXMUSIC/plugins/bot/help.py
+++ DAXXMUSIC/plugins/bot/help.py
@@ -28,7 +28,7 @@ async def helper_private(
         _ = get_string(language)
         keyboard = help_pannel(_, True)
         await update.edit_message_text(
-            _["help_1"].format(SUPPORT_CHAT), reply_markup=keyboard
+            _["help_1"].format(SUPPORT_CHAT, message.from_user.mention), reply_markup=keyboard
         )
     else:
         try:
